a203b19e51e1b69fe19b18abefc8325b6800bf77
ali <ali.ghaieni@gmail.com>
2021-02-25 19:51:49 +0330

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module bitbucket.org/espyrix/tarantulas
 
 go 1.15
+
+require (
+	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/klauspost/compress v1.11.8 // indirect
+	github.com/valyala/fasthttp v1.21.0
+)
--- tarantulas.go
+++ tarantulas.go
@@ -1,5 +1,10 @@
 package tarantulas
 
+import (
+	"fmt"
+	"github.com/valyala/fasthttp"
+)
+
 type Result struct {
 	Asset   string
 	Domain  string
@@ -8,5 +13,23 @@ type Result struct {
 }
 
 func GetContents(domain string, subdomains []string, thread int, httpPorts, httpsPorts []int) {
+	isListening(subdomains[0])
+}
+
+func isListening(url string) bool {
+
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseRequest(req) // <- do not forget to release
+	defer fasthttp.ReleaseResponse(resp) // <- do not forget to release
+
+	req.SetRequestURI(url)
+
+	_ = fasthttp.Do(req, resp)
+
+	bodyBytes := resp.Body()
+	fmt.Println(string(bodyBytes))
+
+	return true
 
 }

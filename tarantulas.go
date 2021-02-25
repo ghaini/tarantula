5402cd5517b6125b16980a87f440d40d5fbcddf6
ali <ali.ghaieni@gmail.com>
2021-02-26 00:25:16 +0330

    add req

--- tarantulas.go
+++ tarantulas.go
@@ -1,32 +1,92 @@
-package tarantulas
+package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/valyala/fasthttp"
+	"net"
+	"regexp"
+	"strings"
 )
 
 type Result struct {
-	Asset   string
-	Domain  string
-	Body    string
-	Headers map[string]interface{}
+	StatusCode int
+	Asset      string
+	Domain     string
+	Body       string
+	Headers    map[string]string
 }
 
-func GetContents(domain string, subdomains []string, thread int, httpPorts, httpsPorts []int) string {
-	return isListening(subdomains[0])
+func main() {
+	fmt.Println(GetContents("", []string{"https://webhook.site/a1bcaec4-149c-4343-b080-723ecb78c999"}, 0, []int{}, []int{}))
+}
+func GetContents(domain string, subdomains []string, thread int, httpPorts, httpsPorts []int) []Result {
+	doRequest(subdomains[0])
+	return []Result{}
 }
 
-func isListening(url string) string {
-
+func doRequest(url string) Result {
 	req := fasthttp.AcquireRequest()
-	resp := fasthttp.AcquireResponse()
-	defer fasthttp.ReleaseRequest(req) // <- do not forget to release
-	defer fasthttp.ReleaseResponse(resp) // <- do not forget to release
-
+	defer fasthttp.ReleaseRequest(req)
 	req.SetRequestURI(url)
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+	err := fasthttp.Do(req, resp)
+	if err != nil {
+		fmt.Printf("Client get failed: %s\n", err)
+		return Result{}
+	}
 
-	_ = fasthttp.Do(req, resp)
+	headers := make(map[string]string)
+	headerString := resp.Header.String()
 
-	bodyBytes := resp.Body()
-	return string(bodyBytes)
+	r, _ := regexp.Compile("(^.+)\\:(.+$)")
+	scanner := bufio.NewScanner(strings.NewReader(headerString))
+	for scanner.Scan() {
+		text := scanner.Text()
+		headers[strings.ToLower(strings.TrimSpace(r.FindStringSubmatch(text)[1]))] = strings.ToLower(strings.TrimSpace(r.FindStringSubmatch(text)[2]))
+	}
 
+
+	return Result{
+		StatusCode: resp.StatusCode(),
+		Asset:      url,
+		Domain:     "",
+		Body:       string(resp.Body()),
+		Headers:    headers,
+	}
+}
+
+func FasthttpHTTPDialer(proxyAddr string) fasthttp.DialFunc {
+	return func(addr string) (net.Conn, error) {
+		fmt.Println(addr)
+		conn, err := fasthttp.Dial(proxyAddr)
+		if err != nil {
+			return nil, err
+		}
+
+		req := "CONNECT " + addr + " HTTP/1.1\r\n"
+		// req += "Proxy-Authorization: xxx\r\n"
+		req += "\r\n"
+
+		if _, err := conn.Write([]byte(req)); err != nil {
+			return nil, err
+		}
+
+		res := fasthttp.AcquireResponse()
+		defer fasthttp.ReleaseResponse(res)
+
+		res.SkipBody = true
+
+		if err := res.Read(bufio.NewReader(conn)); err != nil {
+			conn.Close()
+			return nil, err
+		}
+		if res.Header.StatusCode() != 200 {
+			conn.Close()
+			return nil, fmt.Errorf("could not connect to proxy")
+		}
+
+		return conn, nil
+	}
 }
